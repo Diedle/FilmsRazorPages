7014b168c265c49c3da55b9e88214b065237e290
Dmitry Levchuk <diedle101000@gmail.com>
2023-12-10 22:47:24 +0300

    Identity and comments

---
+++ Films/Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace Films.Models;
+
+public class Comments
+{
+    public int Id { get; set; }
+    
+    public string? Userid { get; set; }
+    
+    public string? Comment { get; set; }
+}
